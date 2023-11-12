54d9e88291f46340e4a4f949600e00ed75381d71
queukat <75810528+queukat@users.noreply.github.com>
2023-11-12 15:48:31 +0400

    Update data_management.py

--- data_management.py
+++ data_management.py
@@ -13,25 +13,57 @@ class UnifiedDataManager:
         self.main_data = pd.DataFrame()
         self.history_data = pd.DataFrame(columns=['column_name', 'old_value', 'new_value', 'timestamp'])
 
-    # Load table method
     def load_table(self, table_name, df):
+        """
+        Load a table into the manager.
+
+        Args:
+            table_name (str): The name of the table to load.
+            df (pd.DataFrame): The dataframe containing the table's data.
+        """
         self.tables[table_name] = df.copy()
 
-    # Merge tables method
     def merge_tables_on_key(self, table1_name, table2_name, key):
+        """
+        Merge two tables on a common key.
+
+        Args:
+            table1_name (str): The name of the first table.
+            table2_name (str): The name of the second table.
+            key (str): The key to merge on.
+
+        Returns:
+            pd.DataFrame: The merged dataframe, or None if either table name is not found.
+        """
         if table1_name in self.tables and table2_name in self.tables:
             return pd.merge(self.tables[table1_name], self.tables[table2_name], on=key, how='outer')
         else:
             return None
 
-    # Data quality method
     def clean_strings(self, df, columns):
+        """
+        Clean and standardize string columns in a dataframe.
+
+        Args:
+            df (pd.DataFrame): The dataframe to clean.
+            columns (list): The list of columns to standardize.
+
+        Returns:
+            pd.DataFrame: The dataframe with cleaned and standardized string columns.
+        """
         for col in columns:
             df[col] = df[col].str.strip().str.lower()
         return df
 
-    # Audit logger methods
     def log_action(self, action, user, details):
+        """
+        Log an action performed by a user.
+
+        Args:
+            action (str): The action performed.
+            user (str): The user who performed the action.
+            details (str): Additional details about the action.
+        """
         self.log = self.log.append({
             "timestamp": pd.Timestamp.now(),
             "action": action,
@@ -40,14 +72,34 @@ class UnifiedDataManager:
         }, ignore_index=True)
 
     def get_log(self):
+        """
+        Get a copy of the action log.
+
+        Returns:
+            pd.DataFrame: A copy of the log dataframe.
+        """
         return self.log.copy()
 
-    # Data versioning methods
     def save_version(self, df):
+        """
+        Save a version of the dataframe.
+
+        Args:
+            df (pd.DataFrame): The dataframe to save.
+        """
         version_hash = hashlib.md5(df.to_string().encode()).hexdigest()
         self.versions.append({"data": df.copy(), "hash": version_hash, "timestamp": pd.Timestamp.now()})
 
     def restore_version(self, version_hash):
+        """
+        Restore a version of the dataframe.
+
+        Args:
+            version_hash (str): The hash of the version to restore.
+
+        Returns:
+            pd.DataFrame: The restored dataframe, or None if not found.
+        """
         for version in self.versions:
             if version["hash"] == version_hash:
                 return version["data"].copy()
